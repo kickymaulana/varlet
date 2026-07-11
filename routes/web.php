ed2c9522ea1f12aadad2ea55ec361c020bd5d96b
kickymaulana <kickymaulana@gmail.com>
2026-07-11 15:12:46 +0700

    redirect

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\AttendanceController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'nama' => 'Developer Keren'
-    ]);
-});
+Route::redirect('/', '/absensi');
 
 Route::get('/absensi', [AttendanceController::class, 'index'])->name('attendance.index');
 Route::post('/absensi/search', [AttendanceController::class, 'search'])->name('attendance.search');
